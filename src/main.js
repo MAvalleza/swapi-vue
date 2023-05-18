c7897acd19d56cc5dd3c5773bb3daf28f2ddd462
Marc Valleza <m.valleza@arcanys.com>
2023-05-18 15:38:16 +0800

    Setup routes and initial page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import HomePage from './pages/HomePage.vue'
 
 // Vuetify
 import 'vuetify/styles'
@@ -8,9 +10,25 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify*{
+const vuetify = createVuetify({
   components,
   directives,
-}
+})
 
-createApp(App).use(vuetify).mount('#app')
+// Vue Router
+
+const routes = [
+  { path: '/', component: HomePage }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+
+app.use(vuetify)
+app.use(router)
+
+app.mount('#app')
