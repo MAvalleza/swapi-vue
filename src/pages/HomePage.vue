2e05940ab1b369e99a25e6067ca77503e8e6f982
Marc Valleza <m.valleza@arcanys.com>
2023-05-18 18:02:22 +0800

    Create homepage component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,4 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const entityTypes = ref([
+  'Planets',
+  'People',
+  'Films',
+  'Starships',
+  'Vehicles',
+  'Species',
+])
+</script>
+
 <template lang="pug">
-div
-  h1 Hello World!
+v-container
+  h1 SWAPI Portal
+  p.my-10 A collection of Star Wars characters, films, and more!
+
+  v-list(v-for="(type, key) in entityTypes")  
+    v-list-item(:key="key")
+      v-list-item-title {{ type }}
 </template>
