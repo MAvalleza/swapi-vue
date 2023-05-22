7d6143e6e5e7ec4f0f258587e583efef55cad66c
Marc Valleza <m.valleza@arcanys.com>
2023-05-22 14:26:30 +0800

    Refactor fetching of entities

--- src/pages/ListPage.vue
+++ src/pages/ListPage.vue
@@ -1,5 +1,28 @@
+<script setup>
+import { useEntities } from '@/stores/entities'
+import { storeToRefs } from 'pinia'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const entitiesStore = useEntities()
+const { entities, people } = storeToRefs(entitiesStore)
+
+const entityType = ref(route.params.entityType)
+
+async function fetchEntities(entityType) {
+  await entitiesStore.fetchEntities(entityType.value)
+}
+
+onMounted(() => {
+  fetchEntities(entityType)
+})
+</script>
+
 <template lang="pug">
-div
+v-container
   h1 List Page
-  p {{  $route.params.entityType }}
+  p {{ entityType }}
+
+  pre {{ entities }}
 </template>
--- src/stores/entities.js
+++ src/stores/entities.js
@@ -1,40 +1,42 @@
 import { defineStore } from 'pinia'
 import queryString from 'query-string'
-import { ref } from 'vue'
 
 const SWAPI_BASE_URL = import.meta.env.VITE_SWAPI_BASE_URL
 
-export const useEntities = defineStore('entities', () => {
-  const people = ref([])
+export const useEntities = defineStore('entities', {
+  state: () => ({
+    entities: [],
+    films: [],
+    people: [],
+    planets: [],
+    species: [],
+    starships: [],
+    vehicles: [],
+  }),
+  actions: {
+    async fetchEntities(entityType, options = {}) {
+      console.log('store invoked')
+      if (!entityType) return [];
+  
+      const { page, search } = options
+      // Remove nullish attributes and turn to query string
+      const params = queryString.stringify({
+        page,
+        search 
+      }, {
+        skipNull: true,
+        skipEmptyString: true
+      })
+  
+      const response = await fetch(`${SWAPI_BASE_URL}/${entityType}/${params}`)
+      const data = await response.json()
+  
+      this[entityType] = data.results || []
+      console.log('entityType', this[entityType])
 
-  function fetchEntities(entityType, options) {
-    if (!entityType) return [];
-
-    // Remove nullish attributes and turn to query string
-    const params = queryString.stringify(options, {
-      skipNull: true,
-      skipEmptyString: true
-    })
-
-    return fetch(`${SWAPI_BASE_URL}/${entityType}/${params}`)
-  }
-
-  async function fetchPeople({
-    page = 1,
-    search,
-  }) {
-    const data = await fetchEntities('people', {
-      page,
-      search
-    })
-
-    people.value = data.results || []
-  }
-
-  return {
-    // Data
-    people,
-    // Methods
-    fetchPeople,
-  }
+      // Assign to `entities` so this will be displayed on the list page
+      this.entities = this[entityType]
+      console.log('entities', this.entities)
+    }
+  },
 })
