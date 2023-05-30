12665f8ab8fefb97a20319fcf92a4e94c289575a
Marc Valleza <m.valleza@arcanys.com>
2023-05-30 11:45:05 +0800

    Do not mutate entity

--- src/pages/DetailsPage.vue
+++ src/pages/DetailsPage.vue
@@ -1,23 +1,23 @@
 <script setup>
 import { useEntity } from '@/stores/entity'
-import { storeToRefs } from 'pinia';
-import { onMounted, ref } from 'vue';
+// import { storeToRefs } from 'pinia';
+import { onMounted, ref, reactive } from 'vue';
 import { useRoute } from 'vue-router'
 
 const entityStore = useEntity()
-const { entity } = storeToRefs(entityStore)
 const route = useRoute()
 
 const category = ref(route.params.category)
 const id = ref(route.params.id)
 
 let loading = ref(false)
+let entity = reactive({})
 
 // Initializer function
 async function initialize() {
   loading.value = true
 
-  await entityStore.fetchEntity(category.value, id.value)
+  entity = await entityStore.fetchEntity(category.value, id.value)
 
   loading.value = false
 }
--- src/stores/entity.js
+++ src/stores/entity.js
@@ -2,14 +2,9 @@ import { defineStore } from 'pinia'
 import { fetchEntity } from '@/webservices/entitiesWebservice'
 
 export const useEntity = defineStore('entity', {
-  state: () => ({
-    entity: {}
-  }),
   actions: {
     async fetchEntity (category, id) {
       const data = await fetchEntity(category, id)
-      this.entity = data
-
       return data
     },
   },
