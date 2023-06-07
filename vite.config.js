6fadb0dd1092f7853b0b779a85cc1b5ea3b63895
Marc Valleza <m.valleza@arcanys.com>
2023-06-07 16:33:18 +0800

    Install vitest

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`
     }
-  }
+  },
+  root: '.'
 })
